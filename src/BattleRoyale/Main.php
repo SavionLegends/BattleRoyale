a1ef3837cea38da17c3a87d3b10036a3f7b1ce44
Savion <saviondeavault@icloud.com>
2018-10-26 10:32:47 -0400

    Upload some files

---
+++ src/BattleRoyale/Main.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 20deavaults
+ * Date: 10/26/18
+ * Time: 10:29 AM
+ */
+
+namespace BattleRoyale;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+
+    }
+
+    public function onDisable(){
+
+    }
+}
